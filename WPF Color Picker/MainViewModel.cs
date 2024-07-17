d52a0ed86c715180d7b22c78f2b2d5e44b91f1c7
Kent Caraway <kcaraway0@gmail.com>
2024-07-17 16:35:51 -0400

    Add project files.

---
+++ WPF Color Picker/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WPF_Color_Picker;
+
+[ObservableObject]
+public partial class MainViewModel
+{
+    [ObservableProperty]
+    private byte _redValue;
+    partial void OnRedValueChanged(byte value) => SetColor();
+    [ObservableProperty]
+    private byte _greenValue;
+    partial void OnGreenValueChanged(byte value) => SetColor();
+    [ObservableProperty]
+    private byte _blueValue;
+    partial void OnBlueValueChanged(byte value) => SetColor();
+    [ObservableProperty]
+    private SolidColorBrush _colorBrush;
+
+
+    [RelayCommand]
+    private void SetColor()
+    {
+        Color color = Color.FromRgb(_redValue, _greenValue, _blueValue);
+        ColorBrush = new SolidColorBrush(color);
+    }
+}
